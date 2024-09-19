d2c14d202fe9e92b294cd3064420f3691c051409
Max Taggart <taggart@digialdiagnostics.com>
2024-09-19 16:30:04 -0600

    Fixed bug that affected multi-line function definitions.

--- src/extension.ts
+++ src/extension.ts
@@ -66,22 +66,26 @@ function docstring_start_lines_for_symbol(symbol: vscode.DocumentSymbol, documen
 	let child_starts = symbol.children.flatMap((child) => docstring_start_lines_for_symbol(child, document));
 	// Now detect if this function/class/method has a docstring, and if so, what
 	// its line range is
-	const first_body_line_number = symbol.range.start.line + 1
-	const body_range = new vscode.Range(new vscode.Position(first_body_line_number, 0), symbol.range.end)
-	const body_text = document.getText(body_range)
-	const docstring_pattern = /^(\s+)?(\"{3}.+?\"{3})/gms
-	const parts = docstring_pattern.exec(body_text);
+	const symbol_text = document.getText(symbol.range)
+	const docstring_pattern = /^(\s+)?(def|class)(.+?:[ \t]*)(\n)(\s+)?(\"{3}.+?\"{3})/gms
+	const parts = docstring_pattern.exec(symbol_text);
 	if (parts === null) {
 		// We couldn't find a docstring in the body
 		return child_starts
 	}
-	// We found a docstring. However, we don't want to fold single-line
-	// docstrings, so we need to figure out how many lines it spans 
-	const docstring_text = parts[2] 
-	const n_lines = docstring_text.split("\n").length
-	if (n_lines > 1) {
-		child_starts.push(first_body_line_number)
+	// We found a docstring. However, we don't know which line it starts on (the
+	// function may be defined over multiple lines) and we don't know how long
+	// the docstring is; we don't want to fold single-line docstrings, so we
+	// need to figure out how many lines it spans.
+	const definition_text = parts[3];
+	const definition_lines = definition_text.split("\n").length
+	const docstring_text = parts[6] 
+	const docstring_lines = docstring_text.split("\n").length
+	if (docstring_lines == 1) {
+		return child_starts
 	}
+	const docstring_start_line = symbol.range.start.line + definition_lines
+	child_starts.push(docstring_start_line)
 	return child_starts
 }
 
